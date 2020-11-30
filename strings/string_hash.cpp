4d284e647560c9482009a0bf8e13e1dd97f8cc67
ferdouszislam <ferdouszislam@gmail.com>
2020-11-30 12:27:38 +0600

    String Hash Update: string_hash.cpp file edit StringHash struct's base, mod member variable, constructor with base mod addition

--- strings/string_hash.cpp
+++ strings/string_hash.cpp
@@ -16,7 +16,7 @@
 using namespace std;
 
 
-///------------------- algorithm function/s to be placed here start -------------------///
+///--------------------------------------------------------- String Hashing start ---------------------------------------------------------///
 
 const int PRIMES[] = {2147462393, 2147462419, 2147462587, 2147462633, 2147462747, 2147463167, 2147463203, 2147463569, 2147463727, 2147463863, 2147464211, 2147464549, 2147464751, 2147465153, 2147465563, 2147465599, 2147465743, 2147465953, 2147466457, 2147466463, 2147466521, 2147466721, 2147467009, 2147467057, 2147467067, 2147467261, 2147467379, 2147467463, 2147467669, 2147467747, 2147468003, 2147468317, 2147468591, 2147468651, 2147468779, 2147468801, 2147469017, 2147469041, 2147469173, 2147469229, 2147469593, 2147469881, 2147469983, 2147470027, 2147470081, 2147470177, 2147470673, 2147470823, 2147471057, 2147471327, 2147471581, 2147472137, 2147472161, 2147472689, 2147472697, 2147472863, 2147473151, 2147473369, 2147473733, 2147473891, 2147473963, 2147474279, 2147474921, 2147474929, 2147475107, 2147475221, 2147475347, 2147475397, 2147475971, 2147476739, 2147476769, 2147476789, 2147476927, 2147477063, 2147477107, 2147477249, 2147477807, 2147477933, 2147478017, 2147478521, 2147478563, 2147478649, 2147479447, 2147479589, 2147480707, 2147480837, 2147480927, 2147480971, 2147481263, 2147481311, 2147481337, 2147481367, 2147481997, 2147482021, 2147482063, 2147482081, 2147482343, 2147482591, 2147483069, 2147483123};
 
@@ -74,6 +74,7 @@ struct StringHash{
 // use this struct in main code
 
     SingleHash sh1, sh2;
+    int base_1, mod_1, base_2, mod_2;
 
     StringHash(string str){
     // Use the SingleHash implementation by providing base & mod
@@ -84,10 +85,24 @@ struct StringHash{
         srand(seed);  /// to avoid getting hacked in CF, comment this line for easier debugging
 
         int q_len = (sizeof(PRIMES) / sizeof(PRIMES[0])) / 4;
-        int base_1 = PRIMES[rand() % q_len];
-        int mod_1  = PRIMES[rand() % q_len + q_len];
-        int base_2 = PRIMES[rand() % q_len + 2 * q_len];
-        int mod_2  = PRIMES[rand() % q_len + 3 * q_len];
+        base_1 = PRIMES[rand() % q_len];
+        mod_1  = PRIMES[rand() % q_len + q_len];
+        base_2 = PRIMES[rand() % q_len + 2 * q_len];
+        mod_2  = PRIMES[rand() % q_len + 3 * q_len];
+
+        sh1 = SingleHash(str, base_1, mod_1);
+        sh2 = SingleHash(str, base_2, mod_2);
+    }
+
+    StringHash(string str, int _base_1, int _mod_1, int _base_2, int _mod_2){
+    // Use the SingleHash implementation with fixed base & mod
+    // useful when multiple strings hash comparison is required
+    // provide previous strings base and mod as parameters
+
+        base_1 = _base_1;
+        mod_1  = _mod_1;
+        base_2 = _base_2;
+        mod_2  = _mod_2;
 
         sh1 = SingleHash(str, base_1, mod_1);
         sh2 = SingleHash(str, base_2, mod_2);
@@ -110,7 +125,7 @@ struct StringHash{
     }
 };
 
-///------------------- algorithm function/s to be placed here end -------------------///
+///---------------------------------------------------------  String Hashing end  ---------------------------------------------------------///
 
 
 void exec(){
